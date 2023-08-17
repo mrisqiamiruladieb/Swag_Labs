b2c7d1f857d6336d2af8a77299310fe66f72193e
adieb <mrizqiamiruladieb@gmail.com>
2023-08-17 16:03:35 +0700

    add success login test

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -10,12 +10,29 @@ import java.time.Duration;
 public class Login extends env_target {
     @Test
     public void main(){
-        //Set driverlocation path
+        //Set driver location path
         System.setProperty("webdriver.chrome.driver","src\\main\\resources\\drivers\\chromedriver.exe");
         //Maximize driver
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         //Set url
         driver.get(baseUrl);
+        //Set waktu tunggu
+        Duration duration = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver, duration);
+        //Set case stop tunggu
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='submit'][@value='Login']"))
+        );
+        //Set element locate
+        driver.findElement(By.name("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//input[@type='submit'][@value='Login']")).click();
+        //Set case stop tunggu
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='title'][contains(text(), 'Products')]"))
+        );
+        //Quit chrome
+        driver.quit();
     }
 }
