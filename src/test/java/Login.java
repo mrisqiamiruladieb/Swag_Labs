798b23e058d92c3a9e8014bbc567bdd6d2b29039
adieb <mrizqiamiruladieb@gmail.com>
2023-08-17 15:51:29 +0700

    success run chrome driver

---
+++ src/test/java/Login.java
@@ -0,0 +1,21 @@
+import config.env_target;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Login extends env_target {
+    @Test
+    public void main(){
+        //Set driverlocation path
+        System.setProperty("webdriver.chrome.driver","src\\main\\resources\\drivers\\chromedriver.exe");
+        //Maximize driver
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        //Set url
+        driver.get(baseUrl);
+    }
+}
